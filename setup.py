1e4f13b3ca338fa099984c32ade2ba40d8e5c504
Yury Kirienko <iurii_kirienko@epam.com>
2017-12-26 17:45:32 +0300

    migration to pytest

--- setup.py
+++ setup.py
@@ -23,12 +23,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         # 'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         # 'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
         # 'Programming Language :: Python :: 3.4',
     ],
-    install_requires=['sympy', 'uncertainties>=3.0.1'],
+    install_requires=['pytest', 'sympy', 'uncertainties>=3.0.1'],
 )
